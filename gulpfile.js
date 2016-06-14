a1e18885d22510ec008a7e432c0015163b615174
Sarvesh Shejwadkar <sarveshshejwadkar@gmail.com>
2016-06-15 01:25:28 +0530

    linters and compressors

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var jshint		= require('gulp-jshint');
+var uglify 		= require('gulp-uglify');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -7,8 +9,20 @@ gulp.task('browser-sync', function() {
     });
 });
 
+gulp.task('js-lint', function() {
+  return gulp.src('./js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
 gulp.task('html-watch', ['browser-sync'], function () {
 	gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['html-watch']);
+gulp.task('js-compress', function() {
+  return gulp.src('./js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['html-watch', 'js-lint', 'js-compress']);
